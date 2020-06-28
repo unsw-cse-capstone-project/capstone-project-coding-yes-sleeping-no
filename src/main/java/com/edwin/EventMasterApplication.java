7dac4f31f74fb752d9accafe6d748331bd7c14a0
edwin <wcs2019inau@gmail.com>
2020-06-28 15:15:31 +1000

    9900 project

---
+++ src/main/java/com/edwin/EventMasterApplication.java
@@ -0,0 +1,13 @@
+package com.edwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edwin/EventMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
